3ae38ca51b8229bd854b0c14a55113607156b473
zjq383688496 <zjq383688496@gmail.com>
2018-05-03 01:27:36 +0800

    no message

--- src/containers/operate/edit/Module/EditContent/Picture/index.js
+++ src/containers/operate/edit/Module/EditContent/Picture/index.js
@@ -12,6 +12,8 @@ import { bindActionCreators } from 'redux'
 import { connect }  from 'react-redux'
 import * as actions from 'actions'
 import { Input } from 'antd'
+import PictureList from './PictureList/PictureList' 
+
 
 class Picture extends React.Component {
 	componentWillMount() {}
@@ -22,19 +24,29 @@ class Picture extends React.Component {
 		let { data, actions } = this.props
 		data.content.routerOption = e.currentTarget.value
 		actions.updateComp(null, data)
-	}
-
+	};
+	add_img = () => {
+		this.addImgModal.show();
+	}; 
 	render() {
 		let { data } = this.props
-		return (
-			<div className="c-picture">
+		return ( 
+			<div className="c-picture"> 
+				<h2>添加素材:</h2>
+				<div className="modules">
+				{
+					data.content.img ? <div className="add_img" onClick={this.add_img}><div className="shadow"><div className="add_text_change">更换图片</div></div><img src={data.content.img} /></div> : <div className="add_img" onClick={this.add_img}><div className="add_text">+</div><div>添加图片</div></div>
+				} 
+				</div>
+				<h2>跳转链接:</h2> 
 				<Input
 					type="textarea"
 					placeholder="请输入要跳转的网址"
 					defaultValue={data.content.routerOption}
 					onBlur={this.handleFocusBlur.bind(this)} 
 				/>
-			</div>
+				 <PictureList ref={com => { this.addImgModal = com; }} />
+			</div> 
 		)
 	}
 }
