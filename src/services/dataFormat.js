6edd7d475ec3234e641e35c4e6f6641671ef76f3
jimmy.zhuang <zhuangjiaqi@rongyi.com>
2018-07-07 11:51:05 +0800

    no message

--- src/services/dataFormat.js
+++ src/services/dataFormat.js
@@ -77,13 +77,14 @@ const dataFormat = {
 								da.map((_, i) => {
 									var dn = _.name, on
 									for (var j = 0, l = org.length; j < l; j++) {
-										var o = org[j],
-											n = o.name,
-											s = o.styleList
+										var o  = org[j],
+											n  = o.name,
+											s1 = _.styleList,
+											s  = o.styleList
+										if (s1.idx === undefined) s1.idx = 0
 										if (dn === n) {
 											on = o
-											_.styleList.idx = _.styleList.idx || 0
-											if (_.styleList.idx === s.idx) break
+											if (s1.idx === s.idx) break
 										}
 									}
 									on = on? on: comp[dn]
